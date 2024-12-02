d23e673efb8ab361ae1353fbd38d7bb1d2f95a2f
Hatcher Banks <hatcherlbanks@gmail.com>
2024-12-01 20:06:23 -0600

    added the basic mysql script for the database. also started implementing the ability to use is, but there is an incredible amount of erros at the moment

--- passwordmanager/src/main/java/module-info.java
+++ passwordmanager/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module cs3360 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive java.sql;
+    requires mysql.connector.java;
 
     opens cs3360 to javafx.fxml;
     exports cs3360;
